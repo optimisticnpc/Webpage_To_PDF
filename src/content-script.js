81e0a495ab3f7aa2af05bc184ad06807224dea30
Joshua Tan <jtan678@aucklanduni.ac.nz>
2023-04-20 23:20:42 +1200

    Added multiple page pdf functionality

--- src/content-script.js
+++ src/content-script.js
@@ -3,28 +3,53 @@ import jsPDF from 'jspdf';
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'convertToPdf') {
     console.log('Message received: convertToPdf');
-    // Extract the text and create a PDF (using an external library)
-    const text = document.body.innerText;
-    createPdf(text);
-  }
-});
-  
-  function createPdf(text) {
-    // This function should use a library like jsPDF to create a PDF.
-    // You can customize the formatting as desired.
-    // Example:
-    // https://github.com/MrRio/jsPDF
-
-    const pdf = new jsPDF();
-    const lines = pdf.splitTextToSize(text, 180); // Wrap text to fit page width
-    pdf.setFontSize(12);
-  
-    let y = 20;
-    for (const line of lines) {
-      pdf.text(15, y, line);
-      y += 7;
+    const content = extractContent();
+    createPdf(content);
+      // const text = document.body.innerText;
+      // createPdf(text);
     }
-  
-    pdf.save('webpage.pdf');
+  });
+
+function extractContent() {
+  // Replace the following selectors with the appropriate ones for the websites you're targeting
+  const selectors = ['article', 'main', '.entry-content', 'entry-content'];
+  // deleted full stop in front of entry-content
+  let content = '';
+
+  for (const selector of selectors) {
+    const element = document.querySelector(selector);
+    if (element) {
+      content = element.innerText;
+      break;
+    }
   }
-  
+
+  return content;
+}
+
+function createPdf(text) {
+  // This function should use a library like jsPDF to create a PDF.
+  // You can customize the formatting as desired.
+  // Example:
+  // https://github.com/MrRio/jsPDF
+  console.log('Creating PDF');
+  const pdf = new jsPDF();
+  const lines = pdf.splitTextToSize(text, 180); // Wrap text to fit page width
+  pdf.setFontSize(12);
+
+  const lineHeight = 7;
+  const pageHeight = pdf.internal.pageSize.height - 40; // Subtract margins (20 top + 20 bottom)
+  let y = 20;
+
+  for (const line of lines) {
+    if (y + lineHeight > pageHeight) {
+      pdf.addPage();
+      y = 20;
+    }
+
+    pdf.text(15, y, line);
+    y += lineHeight;
+  }
+
+  pdf.save('webpage.pdf');
+}
